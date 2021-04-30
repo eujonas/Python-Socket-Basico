268f4ff0f17a4c5f974ce1661e1b6ff3a6e3b8d1
Jonas Lima <eujonaslc@gmail.com>
2021-04-30 14:52:56 -0300

    Create servidor.py

---
+++ servidor.py
@@ -0,0 +1,13 @@
+#servidor​  
+from socket import *
+HOST = '127.0.0.1'              # Endereco IP do Servidor
+PORT = 16022
+print(f'HOST: {HOST} , PORT {PORT}')
+serv = socket(AF_INET, SOCK_STREAM)
+serv.bind((HOST, PORT))
+serv.listen(5)
+while 1:
+    con, adr = serv.accept()
+    while 1:
+        msg = con.recv(1024)
+        print(msg.decode())
